0888aa679273a7d585eee189b759d37ff94cebfc
Elias Machado <prodeng-devops@mercadolibre.com>
2022-12-15 12:58:20 -0300

    Remove cidr_block property

--- variables.tf
+++ variables.tf
@@ -65,7 +65,7 @@ variable "rules" {
     to_port                  = number
     protocol                 = string
     description              = string
-    cidr_blocks              = list(string)
+    cidr_block              = list(string)
     source_security_group_id = string
   }))
   default     = []
